bc8d1daf6d3ac5907fd15e934ed6b0c31a3969a6
ErgoLied <marinakisa100@rambler.ru>
2021-08-19 01:29:53 +0300

    done pr4

---
+++ src/components/car/Car.js
@@ -0,0 +1,18 @@
+export default function Car({item, edit, steal}) {
+    let clickSteal = () => {
+        steal(item.id);
+    }
+    let clickEdit = () => {
+        edit(item);
+    }
+  return (
+    <div className={'car'}>
+        <div>{item.id}</div>
+        <div className={'model'}>{item.model}</div>
+        <div className={'prise'}>{item.price}</div>
+        <div>{item.year}</div>
+        <button onClick={clickEdit}>edit</button>
+        <button onClick={clickSteal}>steal</button>
+    </div>
+  );
+}
---
+++ src/services/cars.service.js
@@ -0,0 +1,32 @@
+// GET http://<IP>/api/v1/cars
+const url='http://195.72.146.25/api/v1/cars';
+const getCars = () => {
+    return fetch(url)
+        .then((response) => response.json());
+}
+const setCar = (car) => {
+    fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(car),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    })
+        .then((response) => response.json())
+}
+const editCar = (id, car) => {
+    fetch((url+'/'+id), {
+        method: 'PUT',
+        body: JSON.stringify(car),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    }).then((response) => response.json())
+}
+const delCar = (id) => {
+    fetch((url+'/'+id),{
+        method: 'DELETE'
+    }).then();
+}
+
+export {getCars, setCar, editCar, delCar};
