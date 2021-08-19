70b22f266f79c916fe81b3cf34cffb82d225a706
ErgoLied <marinakisa100@rambler.ru>
2021-08-19 19:04:31 +0300

    done pr4 part2

--- src/components/posts/Posts.js
+++ src/components/posts/Posts.js
@@ -3,13 +3,17 @@ import {useEffect, useState} from "react";
 import {getPosts} from "../../services/user.api";
 
 export default function Posts({id}) {
-    let [posts, setPosts] = useState([]);
+    const [posts, setPosts] = useState([]);
+
     useEffect(()=>{
-        getPosts(id).then(({data})=>setPosts([...data]));
-    })
+        getPosts(id).then(({data}) => setPosts(data))
+    }, [id])
+
   return (
     <div>
-        {posts.map(value => <Post key={value.id} item={value}/>)}
+        {
+            posts.map(value => <Post key={value.id} item={value}/>)
+        }
     </div>
   );
 }
